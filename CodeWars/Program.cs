f3f60899c61a8c92b6d8c763836cacb768787361
verhovensky <verhovenskyivan@gmail.com>
2022-10-20 22:57:21 +0300

    Created new Kata solution

---
+++ CodeWars/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+       public   static void Main()
+    {
+       static int GetVowelCount(string str) => str.Count(i => "aeiou".Contains(i));
+    }
+}
